5271320ad58c89eb307bb73afc62c09490d34d29
matyklugDebbuged <matyklug@icloud.com>
2020-02-20 16:42:27 +0100

    Added GLFW Window

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,80 @@
-import org.lwjgl.glfw.GLFW;
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class Main {
+
+    private static long window;
+
     public static void main(String[] args) {
         init();
-        Runnable delta = () -> startDeltaUpdate();
-        delta.run();
-        Runnable fixed = () -> startFixedUpdate();
-        fixed.run();
+        Thread delta = new Thread(() -> startDeltaUpdate(), "deltaUpdate");
+        delta.start();
+        Thread fixed = new Thread(() -> startFixedUpdate(), "fixedUpdate");
+        fixed.start();
+        startRenderUpdate();
+        end();
+    }
+
+    private static void end() {
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
     private static void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        if ( window == NULL )
+            throw new RuntimeException("Failed to create the GLFW window");
+
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+                glfwSetWindowShouldClose(window, true);
+        });
+
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+
+        glfwShowWindow(window);
+
+        GL.createCapabilities();
+
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
     }
 
     public static long fixedTime = 2;
@@ -19,7 +84,7 @@ public class Main {
         double lastLoopTime = GLFW.glfwGetTime();
         double lastFPSTime = 0.0;
 
-        while(true) {
+        while(!shouldClose()) {
             double now = GLFW.glfwGetTime();
             double updateLength = now - lastLoopTime;
 
@@ -39,8 +104,24 @@ public class Main {
     private static void updateDelta(double delta) {
     }
 
+    private static void startRenderUpdate() {
+        while(!shouldClose()) {
+            render();
+        }
+    }
+
+    private static void render() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+        glfwSwapBuffers(window); // swap the color buffers
+
+        // Poll for window events. The key callback above will only be
+        // invoked during this call.
+        glfwPollEvents();
+    }
+
     private static void startFixedUpdate() {
-        while(true) {
+        while(!shouldClose()) {
             double frameStart = GLFW.glfwGetTime();
             updateFixed();
             double timeElapsed = GLFW.glfwGetTime() - frameStart;
@@ -57,5 +138,9 @@ public class Main {
 
     private static void updateFixed() {
 
+    }
+
+    public static boolean shouldClose() {
+        return glfwWindowShouldClose(window);
     }
 }
